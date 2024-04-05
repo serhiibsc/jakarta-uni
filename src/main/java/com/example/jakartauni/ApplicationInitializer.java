3213710f5d72194bdb384101cf4b8af96fc021de
serhiibsc <serhii10010@gmail.com>
2024-04-05 14:19:37 +0300

    automated generation of data for "today rates"

--- src/main/java/com/example/jakartauni/ApplicationInitializer.java
+++ src/main/java/com/example/jakartauni/ApplicationInitializer.java
@@ -16,7 +16,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -39,6 +42,22 @@ public class ApplicationInitializer {
 
         loadCurrencyData();
         loadExchangeRateData();
+        loadExchangeRateDataForToday();
+    }
+
+    private void loadExchangeRateDataForToday() {
+        InputStream stream = getClass().getResourceAsStream("/META-INF/exchange_rates_today.csv");
+
+        List<String> lines =
+                new BufferedReader(new InputStreamReader(Objects.requireNonNull(stream), UTF_8))
+                        .lines()
+                        .skip(1)
+                        .collect(Collectors.toList());
+        Collections.shuffle(lines);
+        lines.stream()
+                .map(line -> line.split(","))
+                .map(this::createExchangeRateFromCsvForToday)
+                .forEach(exchangeRateService::createExchangeRate);
     }
 
     private void loadCurrencyData() {
@@ -61,6 +80,17 @@ public class ApplicationInitializer {
                 .map(line -> line.split(","))
                 .map(this::createExchangeRateFromCsv)
                 .forEach(exchangeRateService::createExchangeRate);
+    }
+
+    private ExchangeRate createExchangeRateFromCsvForToday(String[] fields) {
+        Currency target = currencyService.findCurrencyByAbbreviation(fields[2]).orElseThrow();
+        Currency source = currencyService.findCurrencyByAbbreviation(fields[3]).orElseThrow();
+        return ExchangeRate.builder()
+                .date(LocalDate.now())
+                .rate(new BigDecimal(fields[1]))
+                .sourceCurrency(source)
+                .targetCurrency(target)
+                .build();
     }
 
     private ExchangeRate createExchangeRateFromCsv(String[] fields) {
